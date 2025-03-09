d7e7a2ae1d3d003e7e078e6ef086fbca54249bce
Shanjoy Sutradhar <shanjoy.swe@gmail.com>
2025-03-09 14:46:51 +0600

    project setup and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import ProfilePage from "./pages/ProfilePage";
+import RegistrationPage from "./pages/RegistrationPage";
 function App() {
   return (
     <>
-      <div className="text-center capitalize font-extrabold text-red-500">
-        Welcome to vite with tailwind css and react
-      </div>
+      <Routes>
+        <Route element={<HomePage />} path="/" exact />
+        <Route element={<LoginPage />} path="/login" />
+        <Route element={<ProfilePage />} path="/me" />
+        <Route element={<RegistrationPage />} path="/register" />
+        <Route element={<NotFoundPage />} path="*" />
+      </Routes>
     </>
   );
 }
---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,5 @@
+const NotFoundPage = () => {
+  return <div className="text-center font-bold">Page Not Found</div>;
+};
+
+export default NotFoundPage;
